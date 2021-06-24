6e886c7e428233f4e8d20f7e05ff9adecf6ea77a
Ben Horgan <benhorgan68@gmail.com>
2021-06-24 18:03:39 -0400

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import Editor from './Editor';
 import Home from './Home';
 
 console.log('got app');
 
+const prefix = '/map-creator';
+
 const App = () => {
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/maps/:mapId">
-        <Editor />
-      </Route>
+      <Switch>
+        <Route exact path={`${prefix}/`}>
+          <Home />
+        </Route>
+        <Route path={`${prefix}/maps/:mapId`}>
+          <Editor />
+        </Route>
+        <Redirect to={`${prefix}/`} />
+      </Switch>
     </Router>
   );
 };
